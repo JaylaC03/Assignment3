f780f4c69640f457a23a1796f9f446750ae67542
jayla <jayla@MOGUL444>
2025-09-25 20:12:14 -0400

    add c# project files

---
+++ MongoDBConnector.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+
+namespace ClassLibrary1
+{
+    public class MongoDBConnector
+    {
+        private MongoClient client;
+
+        public MongoDBConnector(string connectionString)
+        {
+            client = new MongoClient(connectionString);
+        }
+    }
+}
