d9886e4cacab2c792a3d16f382d797985c2c2c36
jayla <jayla@MOGUL444>
2025-09-26 16:46:13 -0400

    ping method

--- MongoDBConnector.cs
+++ MongoDBConnector.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Bson;
+
 namespace ClassLibrary1
 {
     public class MongoDBConnector
@@ -10,12 +11,14 @@ namespace ClassLibrary1
         {
             client = new MongoClient(connectionString);
         }
+
         public bool Ping()
         {
             try
             {
                 var database = client.GetDatabase("admin");
-                database.RunCommand<MongoDB.Bson.BsonDocument>("{ ping: 1 }");
+                var command = new BsonDocument("ping", 1);
+                database.RunCommand<BsonDocument>(command);
                 return true;
             }
             catch
