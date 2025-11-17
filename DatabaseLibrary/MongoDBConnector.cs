262237109e7af4cc32f96eed57c139bdd350fd25
jayla <jayla@MOGUL444>
2025-11-17 15:00:37 -0500

    feature: add constructor with connection string to MongoDBConnector

--- DatabaseLibrary/MongoDBConnector.cs
+++ DatabaseLibrary/MongoDBConnector.cs
@@ -1,6 +1,29 @@
-﻿namespace DatabaseLibrary;
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
 
-public class Class1
+namespace DatabaseLibrary;
+
+public class MongoDBConnector
 {
+    private MongoClient client;
 
+    public MongoDBConnector(string connectionString)
+    {
+        client = new MongoClient(connectionString);
+    }
+
+    public bool Ping()
+    {
+        try
+        {
+            var database = client.GetDatabase("admin");
+            var command = new BsonDocument("ping", 1);
+            database.RunCommand<BsonDocument>(command);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 }
