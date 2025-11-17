0cdcf264c8e84f80edec455b3bffb49e5bd99c3b
jayla <jayla@MOGUL444>
2025-11-17 15:02:32 -0500

    test: add test for Ping method success case

--- DatabaseLibrary.Tests/MongoDBConnector.Test.cs
+++ DatabaseLibrary.Tests/MongoDBConnector.Test.cs
@@ -1,10 +1,33 @@
-﻿namespace DatabaseLibrary.Tests;
+﻿using Testcontainers.MongoDb;
 
-public class UnitTest1
+namespace DatabaseLibrary.Tests;
+
+public class MongoDBConnectorTests : IAsyncLifetime
 {
-    [Fact]
-    public void Test1()
-    {
+    private MongoDbContainer _mongoContainer = null!;
 
+    public async Task InitializeAsync()
+    {
+        _mongoContainer = new MongoDbBuilder().Build();
+        await _mongoContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _mongoContainer.DisposeAsync();
+    }
+
+    [Fact]
+    public void Ping_WithValidConnection_ReturnsTrue()
+    {
+        // Arrange
+        var connectionString = _mongoContainer.GetConnectionString();
+        var connector = new MongoDBConnector(connectionString);
+
+        // Act
+        var result = connector.Ping();
+
+        // Assert
+        Assert.True(result);
     }
 }
